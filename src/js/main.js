e140afde22eced5b1ebc1fb44c1f1674797e9014
marinaOverchuk <marinaover@ukr.net>
2022-09-12 17:48:27 +0300

    edited

--- src/js/main.js
+++ src/js/main.js
@@ -100,9 +100,9 @@ const partnersSlider = new Swiper('.partners-slider', {
  //close broker-block
 
   $('.broker-block__close').on('click', function() {
-    $(this).parent().addClass('close');
+    $(this).parent().hide();
     // $(p).filter(".broker-comparison__row").eq($(this).index()).hide();
-    $('p').hide()
+    $('p').eq(2).hide()
 
   });
 
