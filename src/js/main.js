0edaa648a12dd97eb0595eaac4e9ccf952ef2539
marinaOverchuk <marinaover@ukr.net>
2022-09-13 17:52:31 +0300

    edited pages

--- src/js/main.js
+++ src/js/main.js
@@ -2,36 +2,34 @@
 
 new WOW().init();
 
-$(document).ready( function() {
+$(document).ready(function () {
 
 
- //header-burger
-  
- $('.header__burger').on('click', function () {
-  
-  if($(this).hasClass('active')){
-    $(this).toggleClass("active");
-    $('body').css('overflow', 'auto');
-    $('header').addClass('header_right');
-  }
-  
-  else{
-    $('header').removeClass('header_right')
-    $(this).toggleClass("active");
-    $('body').css('overflow', 'hidden');
-  }
-  
+  //header-burger
+
+  $('.header__burger').on('click', function () {
+
+    if ($(this).hasClass('active')) {
+      $(this).toggleClass("active");
+      $('body').css('overflow', 'auto');
+      $('header').addClass('header_right');
+    } else {
+      $('header').removeClass('header_right')
+      $(this).toggleClass("active");
+      $('body').css('overflow', 'hidden');
+    }
+
   });
 
 
- // header-scroll
+  // header-scroll
 
   let scrollChange = 50;
-  
+
   function headerScroll(params) {
-    
+
     let scroll = $(window).scrollTop();
-    
+
     if (scroll >= scrollChange) {
       $('header').addClass('header_scroll');
 
@@ -41,75 +39,97 @@ $(document).ready( function() {
     }
   }
 
-  $(document).on('scroll', ()=>{
-    headerScroll();  
+  $(document).on('scroll', () => {
+    headerScroll();
   })
 
   headerScroll();
-  
 
-///Services-Slider
 
-const partnersSlider = new Swiper('.partners-slider', {
+  ///Services-Slider
 
-  slidesPerView: 1,
-  speed: 800,
-  spaceBetween: 30,
-  loop: true,
-  // autoplay: true,
- 
-  breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 1,
-      spaceBetween: 30
+  const partnersSlider = new Swiper('.partners-slider', {
+
+    slidesPerView: 1,
+    speed: 800,
+    spaceBetween: 30,
+    loop: true,
+    // autoplay: true,
+
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 30
+      },
+      576: {
+        slidesPerView: 2,
+        spaceBetween: 30
+      },
+      // when window width is >= 991px
+      991: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      }
     },
-    576: {
-      slidesPerView: 2,
-      spaceBetween: 30
-    },   
-    // when window width is >= 991px
-    991: {
-      slidesPerView: 3,
-      spaceBetween: 30
+    pagination: {
+      el: '.partners__pagination',
+      type: 'bullets',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.home-button-next',
+      prevEl: '.home-button-prev',
     }
-  },
-  pagination: {
-    el: '.partners__pagination',
-    type: 'bullets',
-    clickable: true,
-  },
-  navigation: {
-    nextEl: '.home-button-next',
-    prevEl: '.home-button-prev',
-  }
-});
+  });
 
 
   //tabs
-  $(function() {
-    
-    $('.tabs__caption').on('click', 'li:not(.active)', function() {
+  $(function () {
+
+    $('.tabs__caption').on('click', 'li:not(.active)', function () {
       $(this)
         .addClass('active').siblings().removeClass('active')
         .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
     });
-    
-  });
-  
- //close broker-block
 
-  $('.broker-block__close').on('click', function() {
+  });
+
+  //close broker-block
+
+  $('.broker__block .broker-block__close').on('click', function () {
     $(this).parent().hide();
-    // $(p).filter(".broker-comparison__row").eq($(this).index()).hide();
-    $('p').hide()
-    
-  
+    const ind = $('.broker-block__close').index(this);
+    $('.broker-comparison-row__wrapper .broker-comparison__row').each((i, e) => {
+      const $ths = $(e)
+      $ths.find('p').eq(ind).hide()
+    })
 
   });
 
+
+// header-scroll
+
+
+
+// function stickyScroll(params) {
+
+//   let scroll = $(window).scrollTop();
+
+//   if (scroll >= scrollChange) {
+//     $('.forex-basics__list').addClass('forex-basics-list__fixed');
+
+//   } else {
+//     $('.forex-basics__list').removeClass('forex-basics-list__fixed');
+
+//   }
+// }
+
+// $(document).on('scroll', () => {
+//   stickyScroll();
+// })
+
+// stickyScroll();
 
 
 })
-
-
